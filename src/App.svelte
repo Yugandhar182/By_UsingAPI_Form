8859f2707ba98bae12e255f4a4da84fdfc59a875
Yugandhar182 <yugandhar@recruitly.io>
2023-05-23 17:09:34 +0530

    .

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,67 @@
 <script>
-	export let name;
-</script>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
+	import 'bootstrap/dist/css/bootstrap.min.css';
+  
+	let fullName = '';
+	let email = '';
+	let mobile = '';
+	let file = null;
+  
+	async function handleSubmit() {
+	  // Create a FormData object to send the file and other data
+	  const formData = new FormData();
+	  formData.append('fullName', fullName);
+	  formData.append('email', email);
+	  formData.append('mobile', mobile);
+	  formData.append('file', file);
+  
+	  try {
+		// Make a POST request to your API endpoint
+		const response = await fetch('your-api-endpoint', {
+		  method: 'POST',
+		  body: formData
+		});
+  
+		// Check if the request was successful
+		if (response.ok) {
+		  console.log('Details stored successfully!');
+		  // Reset the form
+		  fullName = '';
+		  email = '';
+		  mobile = '';
+		  file = null;
+		} else {
+		  console.error('Error storing details:', response.statusText);
 		}
+	  } catch (error) {
+		console.error('Error storing details:', error);
+	  }
 	}
-</style>
+  
+	function handleFileChange(event) {
+	  // Retrieve the selected file
+	  file = event.target.files[0];
+	}
+  </script>
+  
+  <main class="container">
+	<form on:submit|preventDefault={handleSubmit}>
+	  <div class="mb-3">
+		<label for="fullName" class="form-label">Full Name:</label>
+		<input type="text" class="form-control" id="fullName" bind:value={fullName} />
+	  </div>
+	  <div class="mb-3">
+		<label for="email" class="form-label">Email:</label>
+		<input type="email" class="form-control" id="email" bind:value={email} />
+	  </div>
+	  <div class="mb-3">
+		<label for="mobile" class="form-label">Mobile:</label>
+		<input type="tel" class="form-control" id="mobile" bind:value={mobile} />
+	  </div>
+	  <div class="mb-3">
+		<label for="file" class="form-label">Choose File:</label>
+		<input type="file" class="form-control" id="file" on:change={handleFileChange} />
+	  </div>
+	  <button type="submit" class="btn btn-primary">Submit</button>
+	</form>
+  </main>
+  
