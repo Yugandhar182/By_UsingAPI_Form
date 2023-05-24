15f98e0e2640fbf108f3e5608417f9c8c87ba0c6
Yugandhar182 <yugandhar@recruitly.io>
2023-05-24 15:28:30 +0530

    .

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,13 @@
 <script>
-	import 'bootstrap/dist/css/bootstrap.min.css';
+	import { onMount } from 'svelte';
+  
 	let fullName = '';
 	let email = '';
 	let mobile = '';
 	let cvFile = null;
 	let fileContent = '';
 	let fileName = '';
+	let formErrors = {};
   
 	async function handleFileChange(event) {
 	  cvFile = event.target.files[0];
@@ -14,7 +16,29 @@
 	}
   
 	async function uploadCV() {
-	  if (cvFile && fileContent && fileName) {
+	  formErrors = {};
+  
+	  if (!fullName.trim()) {
+		formErrors.fullName = 'Full Name is required.';
+	  }
+  
+	  if (!email.trim()) {
+		formErrors.email = 'Email is required.';
+	  } else if (!isValidEmail(email)) {
+		formErrors.email = 'Please enter a valid email address.';
+	  }
+  
+	  if (!mobile.trim()) {
+		formErrors.mobile = 'Mobile is required.';
+	  } else if (!isValidMobile(mobile)) {
+		formErrors.mobile = 'Please enter a valid mobile number.';
+	  }
+  
+	  if (!cvFile) {
+		formErrors.cvFile = 'CV file is required.';
+	  }
+  
+	  if (Object.keys(formErrors).length === 0) {
 		const postData = {
 		  fullName,
 		  email,
@@ -35,6 +59,7 @@
 		  // Handle the response as needed
 		  if (response.ok) {
 			console.log('CV uploaded successfully');
+			resetForm();
 		  } else {
 			console.error('Failed to upload CV');
 		  }
@@ -42,6 +67,18 @@
 		  console.error('Error uploading CV:', error);
 		}
 	  }
+	}
+  
+	function isValidEmail(email) {
+	  // Basic email validation regex
+	  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	  return emailRegex.test(email);
+	}
+  
+	function isValidMobile(mobile) {
+	  // Basic mobile number validation regex
+	  const mobileRegex = /^[0-9]{10}$/;
+	  return mobileRegex.test(mobile);
 	}
   
 	function toBase64(file) {
@@ -52,24 +89,61 @@
 		reader.onerror = error => reject(error);
 	  });
 	}
+  
+	function resetForm() {
+	  fullName = '';
+	  email = '';
+	  mobile = '';
+	  cvFile = null;
+	  fileContent = '';
+	  fileName = '';
+	  formErrors = {};
+	}
+  
+	onMount(() => {
+	  // Reset the file input after submission
+	  const fileInput = document.getElementById('cvFileInput');
+	  if (fileInput) {
+		fileInput.value = '';
+	  }
+	});
   </script>
   
-  
+  <svelte:head>
+	<link
+	  rel="stylesheet"
+	  href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+	/>
+  </svelte:head>
   
   <main class="container">
-	<label class="form-label">
+	<label>
 	  Full Name:
 	  <input type="text" class="form-control" bind:value={fullName} />
+	  {#if formErrors.fullName}
+	  <p class="text-danger">{formErrors.fullName}</p>
+	  {/if}
 	</label>
-	<label class="form-label">
+	<label>
 	  Email:
 	  <input type="email" class="form-control" bind:value={email} />
+	  {#if formErrors.email}
+	  <p class="text-danger">{formErrors.email}</p>
+	  {/if}
 	</label>
-	<label class="form-label">
+	<label>
 	  Mobile:
 	  <input type="tel" class="form-control" bind:value={mobile} />
+	  {#if formErrors.mobile}
+	  <p class="text-danger">{formErrors.mobile}</p>
+	  {/if}
 	</label>
-	<input type="file" class="form-control" accept=".pdf,.doc,.docx" on:change={handleFileChange} />
+	<div class="mb-3">
+	  <input type="file" class="form-control" accept=".pdf,.doc,.docx" id="cvFileInput" on:change={handleFileChange} />
+	  {#if formErrors.cvFile}
+	  <p class="text-danger">{formErrors.cvFile}</p>
+	  {/if}
+	</div>
 	<button class="btn btn-primary" on:click={uploadCV}>Submit</button>
   </main>
   
