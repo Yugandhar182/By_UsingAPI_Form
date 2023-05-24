ef6d03cc35fd8e0e989649fbb74fb568d357af8b
Yugandhar182 <yugandhar@recruitly.io>
2023-05-24 10:45:11 +0530

    .

--- src/App.svelte
+++ src/App.svelte
@@ -1,38 +1,54 @@
 <script>
-	let cvFile = null;
-	let firstName = '';
-	let mobile = '';
+	let fullName = '';
 	let email = '';
+	let mobile = '';
+	let cvFile = null;
+	let fileContent = '';
+	let fileName = '';
+  
+	async function handleFileChange(event) {
+	  cvFile = event.target.files[0];
+	  fileContent = await toBase64(cvFile);
+	  fileName = cvFile.name;
+	}
   
 	async function uploadCV() {
-	  if (cvFile && firstName && mobile && email) {
-		const base64Data = await convertToBase64(cvFile);
+	  if (cvFile && fileContent && fileName) {
 		const postData = {
-		  cv: base64Data,
-		  firstName: firstName,
-		  mobile: mobile,
-		  email: email
+		  fullName,
+		  email,
+		  mobile,
+		  fileContent,
+		  fileName
 		};
   
-		const response = await fetch('https://api.recruitly.io/api/cvsubmit/bytes?apiKey=TEST45684CB2A93F41FC40869DC739BD4D126D77', {
-		  method: 'POST',
-		  headers: {
-			'Content-Type': 'application/json'
-		  },
-		  body: JSON.stringify(postData)
-		});
+		try {
+		  const response = await fetch('https://api.recruitly.io/api/cvsubmit/bytes?apiKey=TEST45684CB2A93F41FC40869DC739BD4D126D77', {
+			method: 'POST',
+			headers: {
+			  'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(postData)
+		  });
   
-		// Handle the response as needed
-		console.log(response);
+		  // Handle the response as needed
+		  if (response.ok) {
+			console.log('CV uploaded successfully');
+		  } else {
+			console.error('Failed to upload CV');
+		  }
+		} catch (error) {
+		  console.error('Error uploading CV:', error);
+		}
 	  }
 	}
   
-	function convertToBase64(file) {
+	function toBase64(file) {
 	  return new Promise((resolve, reject) => {
 		const reader = new FileReader();
+		reader.readAsDataURL(file);
 		reader.onload = () => resolve(reader.result.split(',')[1]);
 		reader.onerror = error => reject(error);
-		reader.readAsDataURL(file);
 	  });
 	}
   </script>
@@ -42,11 +58,19 @@
   </style>
   
   <main class="container">
-	<h1>Details</h1>
-	<input type="file" class="form-control-file" accept=".pdf,.doc,.docx" onchange="cvFile = event.target.files[0]" />
-	<input type="text" class="form-control" placeholder="First Name" value="{firstName}" oninput="firstName = event.target.value" />
-	<input type="text" class="form-control" placeholder="Mobile Number" value="{mobile}" oninput="mobile = event.target.value" />
-	<input type="text" class="form-control" placeholder="Email" value="{email}" oninput="email = event.target.value" />
-	<button class="btn btn-primary" onclick="uploadCV()">Submit</button>
+	<label class="form-label">
+	  Full Name:
+	  <input type="text" class="form-control" bind:value={fullName} />
+	</label>
+	<label class="form-label">
+	  Email:
+	  <input type="email" class="form-control" bind:value={email} />
+	</label>
+	<label class="form-label">
+	  Mobile:
+	  <input type="tel" class="form-control" bind:value={mobile} />
+	</label>
+	<input type="file" class="form-control" accept=".pdf,.doc,.docx" on:change={handleFileChange} />
+	<button class="btn btn-primary" on:click={uploadCV}>Submit</button>
   </main>
   
