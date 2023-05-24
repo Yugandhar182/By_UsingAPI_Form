b5c753aeebb94b990420905543dfbf7dd8e5fe22
Yugandhar182 <yugandhar@recruitly.io>
2023-05-24 09:14:23 +0530

    .

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,52 @@
 <script>
 	let cvFile = null;
-  
-	function handleFileChange(event) {
-	  cvFile = event.target.files[0];
-	}
+	let firstName = '';
+	let mobile = '';
+	let email = '';
   
 	async function uploadCV() {
-	  if (cvFile) {
-		const formData = new FormData();
-		formData.append('file', cvFile);
+	  if (cvFile && firstName && mobile && email) {
+		const base64Data = await convertToBase64(cvFile);
+		const postData = {
+		  cv: base64Data,
+		  firstName: firstName,
+		  mobile: mobile,
+		  email: email
+		};
   
-		const response = await fetch('https://api.recruitly.io/api/cvsubmit/multipart?apiKey=TEST45684CB2A93F41FC40869DC739BD4D126D77', {
+		const response = await fetch('/api/upload', {
 		  method: 'POST',
-		  body: formData
+		  headers: {
+			'Content-Type': 'application/json'
+		  },
+		  body: JSON.stringify(postData)
 		});
   
 		// Handle the response as needed
-		if (response.ok) {
-		  console.log('CV uploaded successfully');
-		} else {
-		  console.error('CV upload failed');
-		}
+		console.log(response);
 	  }
+	}
+  
+	function convertToBase64(file) {
+	  return new Promise((resolve, reject) => {
+		const reader = new FileReader();
+		reader.onload = () => resolve(reader.result.split(',')[1]);
+		reader.onerror = error => reject(error);
+		reader.readAsDataURL(file);
+	  });
 	}
   </script>
   
-  <main>
-	<h1>Upload CV</h1>
-	<input type="file" accept=".pdf,.doc,.docx" on:change={handleFileChange} />
-	<button on:click={uploadCV}>Upload</button>
+  <style>
+	@import 'bootstrap/dist/css/bootstrap.min.css';
+  </style>
+  
+  <main class="container">
+	<h1>Details</h1>
+	<input type="file" class="form-control-file" accept=".pdf,.doc,.docx" onchange="cvFile = event.target.files[0]" />
+	<input type="text" class="form-control" placeholder="First Name" value="{firstName}" oninput="firstName = event.target.value" />
+	<input type="text" class="form-control" placeholder="Mobile Number" value="{mobile}" oninput="mobile = event.target.value" />
+	<input type="text" class="form-control" placeholder="Email" value="{email}" oninput="email = event.target.value" />
+	<button class="btn btn-primary" onclick="uploadCV()">Submit</button>
   </main>
   
